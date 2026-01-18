cb91f563e0dd607a0252f978d2b981e2bdfb2250
Wasii25 <wasiullahrafeeq.s@gmail.com>
2026-01-18 21:37:54 +0530

    ServerSocket class

--- src/GreetClient.java
+++ src/GreetClient.java
@@ -1,2 +1,44 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GreetClient {
+    private Socket clientSocket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String message) throws IOException {
+        out.println(message);
+        String response = in.readLine();
+        return response;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 4444);
+        String resp1 = client.sendMessage("hello");
+        String resp2 = client.sendMessage("world");
+        String resp3 = client.sendMessage("!");
+        String resp4 = client.sendMessage(".");
+
+        assertEquals("hello", resp1);
+        assertEquals("world", resp2);
+        assertEquals("!", resp3);
+        assertEquals("good bye", resp4);
+    }
 }
