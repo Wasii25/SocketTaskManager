29ebd03ccad000d865c646e89706d73325019f41
Wasii25 <wasiullahrafeeq.s@gmail.com>
2026-01-20 00:13:17 +0530

    phase 3 : beginning

--- src/domain/Task.java
+++ src/domain/Task.java
@@ -20,7 +20,7 @@ public class Task {
     public TaskStatus getStatus() {
         return status;
     }
-    public void setStatus(String status) {
+    public void setStatus(TaskStatus status) {
         this.status = status;
     }
 
